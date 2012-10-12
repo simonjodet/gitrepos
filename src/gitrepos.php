ad48b02012200edc7d5d5861c725c36319d69fe5
Simon Jodet <simon@jodet.com>
2012-10-12 20:49:54 +0200

    Creating routes for repositories

--- src/gitrepos.php
+++ src/gitrepos.php
@@ -1,5 +1,56 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-$gitrepos = new Silex\Application();
-echo 'Hello World!';
-return $gitrepos;
+
+$app = new \Silex\Application();
+$app['debug'] = true;
+
+$app->get(
+    '/{username}',
+    function (\Silex\Application $app, $username)
+    {
+        return 'List of repositories for user ' . $username;
+    });
+
+$app->get(
+    '/{username}/add',
+    function (\Silex\Application $app, $username)
+    {
+        return 'Create repository form for user ' . $username;
+    });
+
+$app->post(
+    '/{username}/create',
+    function (\Silex\Application $app, $username)
+    {
+        return 'Create repository for user ' . $username;
+    });
+
+$app->get(
+    '/{username}/{reponame}/',
+    function (\Silex\Application $app, $username, $reponame)
+    {
+        return 'Details for  ' . $username . '/' . $reponame;
+    });
+
+$app->get(
+    '/{username}/{reponame}/edit',
+    function (\Silex\Application $app, $username, $reponame)
+    {
+        return 'Repository edition form for  ' . $username . '/' . $reponame;
+    });
+
+$app->post(
+    '/{username}/{reponame}/update',
+    function (\Silex\Application $app, $username, $reponame)
+    {
+        return 'Repository edition for  ' . $username . '/' . $reponame;
+    });
+
+$app->post(
+    '/{username}/{reponame}/delete',
+    function (\Silex\Application $app, $username, $reponame)
+    {
+        return 'Repository deletion of  ' . $username . '/' . $reponame;
+    });
+
+return $app;
---
+++ tests/web_test_cases/RepositoryRoutesTest.php
@@ -0,0 +1,82 @@
+<?php
+use \Silex\WebTestCase;
+
+class RepositoryRoutesTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../../src/gitrepos.php';
+        $app['debug'] = true;
+        unset($app['exception_handler']);
+        return $app;
+    }
+
+    public function test_repositories_list_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/simon');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_repositories_create_form_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/simon/add');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_repositories_create_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('POST', '/simon/create');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_repository_details_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/simon/reponame/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_naming_a_repository_with_repositories_actions_works()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/simon/add/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+
+        $client = $this->createClient();
+        $client->request('GET', '/simon/create/');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_repository_edit_form_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('GET', '/simon/reponame/edit');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_repository_update_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('POST', '/simon/reponame/update');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+
+    public function test_repository_deletion_route_exists()
+    {
+        $client = $this->createClient();
+        $client->request('POST', '/simon/reponame/delete');
+
+        $this->assertTrue($client->getResponse()->isOk());
+    }
+}

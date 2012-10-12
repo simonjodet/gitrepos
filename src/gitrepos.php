32a848f28743215d136c5f6d1c5146372d54b3d6
Simon Jodet <simon@jodet.com>
2012-10-12 19:48:44 +0200

    Gitrepos first commit
    * README file is ready for Github use
    * composer JSON file contains the Silex dependency
    * a webserver.sh file helps start the PHP 5.4 web server
    * Gitrepos displays "Hello World!"
    * Business code ("src") is separated from the web accessible folder ("web")
    * .gitignore set to ignore composer artifacts

---
+++ src/gitrepos.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+$gitrepos = new Silex\Application();
+echo 'Hello World!';
+return $gitrepos;
---
+++ webserver.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+php -S localhost:8000 -t web web/index.php

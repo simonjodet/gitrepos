945a20c99a12d4171e8ebda474898b6337c0ed41
Simon Jodet <simon@jodet.com>
2012-10-19 20:53:04 +0200

    Creating Database helper
    It can create a versioned schema of the db,
    it can list the existing tables,
    drop a table,
    and reset the whole db (will be used for tests)

---
+++ src/Gitrepos/Database.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Gitrepos;
+
+class Database
+{
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    private $conn;
+
+    public function __construct(\Silex\Application $app)
+    {
+        $this->conn = $app['db'];
+    }
+
+    public function getSchemas()
+    {
+        $Schema = new \Doctrine\DBAL\Schema\Schema();
+
+        $schemas = array(
+            1 => function() use($Schema)
+            {
+                $systemTable = $Schema->createTable('system');
+                $systemTable->addColumn('system_key', 'string', array('length' => 255));
+                $systemTable->addColumn('value', 'string', array('length' => 255));
+                $systemTable->addUniqueIndex(array('system_key'), 'unique_system_key');
+
+                $usersTable = $Schema->createTable('users');
+                $usersTable->addColumn('username', 'string', array('length' => 64));
+                $usersTable->addColumn('email', 'string', array('length' => 255));
+                $usersTable->addColumn('pwd', 'string', array('length' => 255));
+                $usersTable->addUniqueIndex(array('username'), 'unique_users_nickname');
+                $usersTable->addUniqueIndex(array('email'), 'unique_users_email');
+                $usersTable->setPrimaryKey(array('username'));
+
+                return $Schema;
+            }
+        );
+        return $schemas;
+    }
+
+    public function getSchemaSql($version = null)
+    {
+        $schema_versions = $this->getSchemas();
+
+        if (is_null($version))
+        {
+            $version = key(array_slice($schema_versions, -1, 1, TRUE));
+        }
+        if (!array_key_exists($version, $schema_versions))
+        {
+            throw new \Exception('Unknown schema version');
+        }
+        $schema = $schema_versions[$version]();
+
+        $SchemaManager = $this->conn->getSchemaManager();
+
+        if ($SchemaManager->tablesExist('system') && count($this->conn->fetchAll("SELECT * FROM system WHERE system_key = 'database_schema_version';")) > 0)
+        {
+            $database_schema_version_query = "UPDATE system SET value = " . $version . " WHERE system_key = 'database_schema_version'";
+        }
+        else
+        {
+            $database_schema_version_query = "INSERT INTO system (system_key, value) VALUES ('database_schema_version', " . $version . ")";
+        }
+
+        $platform = $this->conn->getDatabasePlatform();
+        $queries = $schema->toSql($platform);
+        $queries[] = $database_schema_version_query;
+        return $queries;
+    }
+
+    public function listTables()
+    {
+        $tables = array();
+        $SchemaManager = $this->conn->getSchemaManager();
+
+        foreach ($SchemaManager->listTables() as $table)
+        {
+            $tables[] = $table->getName();
+        }
+        return $tables;
+    }
+
+    public function dropTable($table)
+    {
+        $this->conn->query("DROP TABLE IF EXISTS '" . $table . "';");
+    }
+
+    public function reset($version = null)
+    {
+        $tables = $this->listTables();
+        foreach ($tables as $table)
+        {
+            $this->dropTable($table);
+        }
+
+        $schema = $this->getSchemaSql($version);
+        foreach ($schema as $query)
+        {
+            $this->conn->query($query);
+        }
+
+    }
+}
---
+++ tests/UnitTests/DatabaseTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Tests\UnitTests;
+
+class DatabaseTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_getSchema_throws_error_on_unknown_version()
+    {
+        $this->setExpectedException('\Exception', 'Unknown schema version');
+
+        $app = new \Silex\Application();
+        $app['db'] = '';
+        $Database = new \Gitrepos\Database($app);
+
+        $Database->getSchemaSql(0);
+    }
+
+    public function test_getSchema_gives_the_latest_version_by_default()
+    {
+        $app = new \Silex\Application();
+
+        $ConnectionMock = \Mockery::mock('\Doctrine\DBAL\Connection');
+        $ConnectionMock
+            ->shouldReceive('getSchemaManager->tablesExist');
+        $ConnectionMock
+            ->shouldReceive('getDatabasePlatform')
+            ->andReturn(new \Doctrine\DBAL\Platforms\SqlitePlatform());
+        $ConnectionMock
+            ->shouldReceive('toSql');
+        $app['db'] = $ConnectionMock;
+        $Database = \Mockery::mock('\Gitrepos\Database[getSchemas]', array($app));
+        $Database
+            ->shouldReceive('getSchemas')
+            ->once()
+            ->andReturn(array(
+            1 => function()
+            {
+                $SchemaMock1 = \Mockery::mock();
+                $SchemaMock1
+                    ->shouldReceive('toSql')
+                    ->andReturn(array('SQL query 1'));
+                return $SchemaMock1;
+            },
+            2 => function()
+            {
+                $SchemaMock2 = \Mockery::mock();
+                $SchemaMock2
+                    ->shouldReceive('toSql')
+                    ->once()
+                    ->andReturn(array('SQL query 2'));
+                return $SchemaMock2;
+            }
+
+        ));
+
+        $this->assertEquals(
+            array(
+                'SQL query 2',
+                "INSERT INTO system (system_key, value) VALUES ('database_schema_version', 2)"
+            ),
+            $Database->getSchemaSql()
+        );
+    }
+
+    public function test_getSchemas_return_an_array_of_callables()
+    {
+        $app = new \Silex\Application();
+        $app['db'] = '';
+
+        $Database = new \Gitrepos\Database($app);
+        $schemas = $Database->getSchemas();
+
+        $this->assertTrue(is_array($schemas));
+        $this->assertTrue(count($schemas) > 0);
+
+        foreach ($schemas as $schema)
+        {
+            if (!is_callable($schema))
+            {
+                $this->fail('All schemas should be callable');
+            }
+        }
+    }
+
+    public function test_listTables_returns_tables_name()
+    {
+        $app = new \Silex\Application();
+
+        $ConnectionMock = \Mockery::mock('\Doctrine\DBAL\Connection');
+
+        $TableMock = \Mockery::mock('\Doctrine\DBAL\Schema\Table');
+        $TableMock
+            ->shouldReceive('getName')
+            ->andReturn('system', 'users');
+
+        $ConnectionMock
+            ->shouldReceive('getSchemaManager->listTables')
+            ->once()
+            ->andReturn(
+            array(
+                $TableMock,
+                $TableMock
+            )
+        );
+
+        $app['db'] = $ConnectionMock;
+        $Database = new \Gitrepos\Database($app);
+        $this->assertEquals(
+            array('system', 'users'),
+            $Database->listTables()
+        );
+    }
+
+    public function test_dropTable_execute_the_right_query()
+    {
+        $app = new \Silex\Application();
+
+        $ConnectionMock = \Mockery::mock('\Doctrine\DBAL\Connection');
+        $ConnectionMock
+            ->shouldReceive('query')
+            ->with("DROP TABLE IF EXISTS 'my_table';")
+            ->once();
+        $app['db'] = $ConnectionMock;
+
+        $Database = new \Gitrepos\Database($app);
+
+        $Database->dropTable('my_table');
+    }
+
+    public function test_reset_list_existing_tables_deletes_them_then_create_a_new_schema()
+    {
+        $app = new \Silex\Application();
+
+        $ConnectionMock = \Mockery::mock('\Doctrine\DBAL\Connection');
+        $ConnectionMock
+            ->shouldReceive('query')
+            ->with('SQL query 1')
+            ->once()
+            ->ordered('reset');
+        $ConnectionMock
+            ->shouldReceive('query')
+            ->with('SQL query 2')
+            ->once()
+            ->ordered('reset');
+        $app['db'] = $ConnectionMock;
+
+        $Database = \Mockery::mock('\Gitrepos\Database[listTables,dropTable,getSchemaSql]', array($app));
+        $Database
+            ->shouldReceive('listTables')
+            ->once()
+            ->ordered('reset')
+            ->andReturn(array('system', 'users'));
+        $Database
+            ->shouldReceive('dropTable')
+            ->with('system')
+            ->once()
+            ->ordered('reset');
+        $Database
+            ->shouldReceive('dropTable')
+            ->with('users')
+            ->once()
+            ->ordered('reset');
+        $Database
+            ->shouldReceive('getSchemaSql')
+            ->with(1)
+            ->once()
+            ->ordered('reset')
+            ->andReturn(array('SQL query 1', 'SQL query 2'));
+
+        $Database->reset(1);
+    }
+}

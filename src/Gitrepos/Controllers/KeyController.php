72ded2d42a9df0f1440aa042877afa2f32a0e159
Simon Jodet <simon@jodet.com>
2013-01-04 15:11:08 +0100

    Build key add form

--- src/Gitrepos/Controllers/KeyController.php
+++ src/Gitrepos/Controllers/KeyController.php
@@ -3,13 +3,39 @@
 namespace Gitrepos\Controllers;
 
 use \Silex\Application,
-    \Symfony\Component\HttpFoundation\Request;
+    \Symfony\Component\HttpFoundation\Request,
+    \Symfony\Component\Validator\Constraints as Assert;
 
 
 class KeyController
 {
     public function addAction(Request $request, Application $app)
     {
-        return '';
+        /**
+         * @var $form \Symfony\Component\Form\Form
+         */
+        $form = $app['form.factory']->createBuilder('form')
+            ->add(
+            'title',
+            'text',
+            array(
+                'constraints' => array(
+                    new Assert\MinLength(1),
+                    new Assert\MaxLength(128)
+                )
+            )
+        )
+            ->add(
+            'key',
+            'text',
+            array(
+                'constraints' => array(
+                    new Assert\MinLength(1),
+                    new Assert\MaxLength(512)
+                )
+            )
+        )
+            ->getForm();
+        return $app['twig']->render('key/add.twig', array('form' => $form->createView()));
     }
 }
